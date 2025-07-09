a89ff8b817a4af66e04986b72d9aa592bb9b1405
Flavio Moreni <PF0954@fiap.com.br>
2025-07-08 21:56:47 -0300

    first commit

---
+++ Fiap.Web.Donation5/Controllers/ClienteController.cs
@@ -0,0 +1,26 @@
+﻿using Fiap.Web.Donation5.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Web.Donation5.Controllers
+{
+    public class ClienteController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(ClienteModel clienteModel)
+        {
+            // Salva os dados no banco de dados
+
+            // Consultando o identificador no banco de dados
+            clienteModel.ClienteId = new Random().Next();
+
+            return View("PostSucesso",clienteModel);
+        }
+
+    }
+}
---
+++ Fiap.Web.Donation5/Models/ClienteModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Fiap.Web.Donation5.Models
+{
+    public class ClienteModel
+    {
+        public int ClienteId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Sobrenome { get; set; }
+
+        public string Email { get; set; }
+
+    }
+}
